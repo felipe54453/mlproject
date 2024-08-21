162c5f97e75fb560bc3d25452c55d0428590b91f
pimpas <felipedmlacombe@gmail.com>
2024-08-21 13:49:15 -0300

    hyperparameter

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -8,7 +8,6 @@ from sklearn.ensemble import (
     GradientBoostingRegressor,
     RandomForestRegressor,
 )
-
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
@@ -17,7 +16,6 @@ from xgboost import XGBRegressor
 
 from src.exception import CustomException
 from src.logger import logging
-
 from src.utils import save_object, evaluate_models
 
 @dataclass
@@ -28,14 +26,14 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self,train_array,test_array):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
-            logging.info('splitting train array and test array input data')
-            X_train, y_train,X_test,y_test = (
-                train_array[:,:-1],
-                train_array[:,-1],
-                test_array[:,:-1],
-                test_array[:,-1]
+            logging.info('Splitting train and test arrays into input features and target variable')
+            X_train, y_train, X_test, y_test = (
+                train_array[:, :-1],
+                train_array[:, -1],
+                test_array[:, :-1],
+                test_array[:, -1]
             )
 
             models = {
@@ -48,20 +46,78 @@ class ModelTrainer:
                 "catboost": CatBoostRegressor(),
             }
 
-            model_repor:dict=evaluate_models(X_train=X_train,
-                                            y_train=y_train,
-                                            X_test=X_test,
-                                            y_test=y_test,
-                                            models=models
-                                            )
-            
-            best_model_score = max([model['test_score'] for model in model_repor.values()])
-            best_model_name = [model_name for model_name, model in model_repor.items() if model['test_score'] == best_model_score][0]
+            params = {
+                "decision_tree": {
+                    'criterion': ['absolute_error', 'friedman_mse'],
+                    'splitter': ['best'],
+                    'max_features': ['sqrt'],
+                    'max_depth': [10, 20],
+                    'min_samples_split': [2, 5],
+                    'min_samples_leaf': [1, 2],
+                },
+                "random_forest": {
+                    'criterion': ['absolute_error', 'squared_error','friedman_mse'],
+                    'max_features': ['sqrt'],
+                    'n_estimators': [50, 100],
+                    'max_depth': [10, 20],
+                    'min_samples_split': [2, 5],
+                    'min_samples_leaf': [1, 2],
+                },
+                "gradient_boosting": {
+                    'loss': ['ls', 'huber'],
+                    'learning_rate': [0.1, 0.01],
+                    'subsample': [0.7, 0.9],
+                    'criterion': ['friedman_mse'],
+                    'max_features': ['sqrt'],
+                    'n_estimators': [50, 100],
+                    'max_depth': [3, 5],
+                    'min_samples_split': [2, 5],
+                    'min_samples_leaf': [1, 2],
+                },
+                "linear_regression": {},
+                "xgboost": {
+                    'learning_rate': [0.1, 0.01],
+                    'n_estimators': [50, 100],
+                    'max_depth': [3, 5],
+                    'subsample': [0.8, 1.0],
+                    'colsample_bytree': [0.8, 1.0],
+                },
+                "catboost": {
+                    'depth': [6, 8],
+                    'learning_rate': [0.05, 0.1],
+                    'iterations': [100, 200],
+                    'l2_leaf_reg': [3, 5],
+                },
+                "ada_boost": {
+                    'learning_rate': [0.1, 0.5],
+                    'n_estimators': [50, 100],
+                    'loss': ['linear'],
+                    'estimator': [DecisionTreeRegressor(max_depth=1)],
+                }
+            }
+
+
+
+            model_report = evaluate_models(
+                X_train=X_train,
+                y_train=y_train,
+                X_test=X_test,
+                y_test=y_test,
+                models=models,
+                params=params
+            )
+
+            best_model_score = max([model['test_score'] for model in model_report.values()])
+            best_model_name = [model_name for model_name, model in model_report.items() if model['test_score'] == best_model_score][0]
             best_model = models[best_model_name]
 
             if best_model_score < 0.6:
                 raise CustomException('Best model score is less than 0.6', sys)
+
+            # Logging best model and its hyperparameters
             logging.info(f'Best model is {best_model_name} with test score {best_model_score}')
+            best_params = model_report[best_model_name]['best_params']
+            logging.info(f'Best hyperparameters for {best_model_name}: {best_params}')
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,9 @@ import dill
 from src.logger import logging
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+
 
 
 def save_object(file_path, obj):
@@ -22,20 +25,45 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
-    
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     '''
-    this function is responsible for evaluating models
+    This function is responsible for evaluating models.
     '''
     try:
         model_report = {}
         for model_name, model in models.items():
+            # Get the parameters for the specific model
+            model_params = params.get(model_name, {})
+            
+            if model_params:  # If there are parameters to tune
+                # Perform grid search with cross-validation
+                grid_search = GridSearchCV(model, model_params, cv=5)
+                grid_search.fit(X_train, y_train)
+                
+                # Update the model with the best found parameters
+                model.set_params(**grid_search.best_params_)
+            else:  # If no parameters to tune, fit the model directly
+                model.fit(X_train, y_train)
+
+            # Fit the model on the entire training set
             model.fit(X_train, y_train)
+
+            # Predict using the fitted model
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
+            
+            # Calculate R2 scores
             train_score = r2_score(y_train, y_train_pred)
             test_score = r2_score(y_test, y_test_pred)
-            model_report[model_name] = {'train_score': train_score, 'test_score': test_score}
+            
+            # Store the results and best parameters in the model report
+            model_report[model_name] = {
+                'train_score': train_score,
+                'test_score': test_score,
+                'best_params': grid_search.best_params_ if model_params else None
+            }
+        
         return model_report
 
     except Exception as e:
